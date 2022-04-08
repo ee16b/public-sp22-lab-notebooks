b66ae937d6cb4af8348f72ee7b592471f3c1af7f
ericyang8484 <ericyang8484@gmail.com>
2022-04-07 22:32:48 -0700

    lab 9 hands-on spring 2022 release

---
+++ lab9_handson_sp22/classify/classify.ino
@@ -0,0 +1,247 @@
+/*
+ * classify.ino
+ *
+ * EE16B Spring 2016
+ * Emily Naviasky & Nathaniel Mailoa
+ *
+ * EE 16B Fall 2017
+ * Andrew Blatner
+ *
+ */
+
+#define MIC_INPUT                   P6_0
+
+#define SIZE                        2752
+#define SIZE_AFTER_FILTER           172
+#define ADC_TIMER_MS                0.35
+
+/*---------------------------*/
+/*      CODE BLOCK PCA1      */
+/*---------------------------*/
+
+// Enveloping and threshold constants
+#define SNIPPET_SIZE                70
+#define PRELENGTH                   5
+#define THRESHOLD                   0.5
+
+#define EUCLIDEAN_THRESHOLD         0
+#define LOUDNESS_THRESHOLD          0
+
+/*---------------------------*/
+/*      CODE BLOCK PCA2      */
+/*---------------------------*/
+
+float pca_vec1[SNIPPET_SIZE] = ;
+float pca_vec2[SNIPPET_SIZE] = ;
+float pca_vec3[SNIPPET_SIZE] = ;
+float projected_mean_vec[3] = ;
+float centroid1[3] = ;
+float centroid2[3] = ;
+float centroid3[3] = ;
+float centroid4[3] = ;
+float* centroids[4] = {
+  (float *) &centroid1, (float *) &centroid2,
+  (float *) &centroid3, (float *) &centroid4
+};
+
+/*---------------------------*/
+/*---------------------------*/
+/*---------------------------*/
+
+float result[SNIPPET_SIZE] = {0};
+float proj1 = 0;
+float proj2 = 0;
+float proj3 = 0;
+
+// Data array and index pointer
+int re[SIZE] = {0};
+volatile int re_pointer = 0;
+
+/*---------------------------*/
+/*       Norm functions      */
+/*---------------------------*/
+
+// Compute the L2 norm of (dim1, dim2) and centroid
+// input: dim1: 1st dimension coordinate
+//        dim2: 2nd dimension coordinate
+//        centroid: size-2 array containing centroid coordinates
+// output: L2 norm (Euclidean distance) between point and centroid
+float l2_norm(float dim1, float dim2, float* centroid) {
+  return sqrt(pow(dim1-centroid[0],2) + pow(dim2-centroid[1],2));
+}
+
+// Compute the L2 norm of (dim1, dim2, dim3) and centroid
+// input: dim1: 1st dimension coordinate
+//        dim2: 2nd dimension coordinate
+//        dim3: 3rd dimension coordinate
+//        centroid: size-3 array containing centroid coordinates
+// output: L2 norm (Euclidean distance) between point and centroid
+float l2_norm3(float dim1, float dim2, float dim3, float* centroid) {
+  return sqrt(pow(dim1-centroid[0],2) + pow(dim2-centroid[1],2) + pow(dim3-centroid[2],2));
+}
+
+void setup(void) {
+  Serial.begin(38400);
+
+  pinMode(MIC_INPUT, INPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
+  re_pointer = 0;
+  reset_blinker();
+  setTimer();
+}
+
+void loop(void) {
+  if (re_pointer == SIZE) {
+    digitalWrite(RED_LED, LOW);
+
+    // Apply enveloping function and get snippet with speech.
+    // Do classification only if loud enough.
+    if (envelope(re, result)) {
+
+      // Reset projection result variables declared above
+      proj1 = 0;
+      proj2 = 0;
+      proj3 = 0;
+
+      /*---------------------------*/
+      /*      CODE BLOCK PCA3      */
+      /*---------------------------*/
+
+      // Project 'result' onto the principal components
+      // Hint: 'result' is an array
+      // Hint: do this entire operation in 1 loop by replacing the '...'
+      // YOUR CODE HERE
+      for (int i = 0; i < SNIPPET_SIZE; i++) {
+          proj1 += ...;
+          proj2 += ...;
+          proj3 += ...;
+      }
+
+      // Demean the projection
+      proj1 -= ...;
+      proj2 -= ...;
+      proj3 -= ...;
+
+      // Classification
+      // Use the function 'l2_norm' defined above
+      // ith centroid: 'centroids[i]'
+      float best_dist = 999999;
+      int best_index = -1;
+      // YOUR CODE HERE
+
+
+      // Compare 'best_dist' against the 'EUCLIDEAN_THRESHOLD' and print the result
+      // If 'best_dist' is less than the 'EUCLIDEAN_THRESHOLD', the recording is a word
+      // Otherwise, the recording is noise
+      // YOUR CODE HERE
+
+
+      /*---------------------------*/
+      /*---------------------------*/
+      /*---------------------------*/
+    }
+    else {
+      Serial.println("Below LOUDNESS_THRESHOLD.");
+    }
+
+    delay(2000);
+    re_pointer = 0;
+  }
+}
+
+// Enveloping function with thresholding and normalizing,
+// returns snippet of interest (containing speech)
+bool envelope(int* data, float* data_out) {
+  int32_t avg = 0;
+  float maximum = 0;
+  int32_t total = 0;
+  int block;
+
+  // Apply enveloping filter while finding maximum value
+  for (block = 0; block < SIZE_AFTER_FILTER; block++) {
+    avg = 0;
+    for (int i = 0; i < 16; i++) {
+      avg += data[i+block*16];
+    }
+    avg = avg >> 4;
+    data[block] = abs(data[block*16] - avg);
+    for (int i = 1; i < 16; i++) {
+      data[block] += abs(data[i+block*16] - avg);
+    }
+    if (data[block] > maximum) {
+      maximum = data[block];
+    }
+  }
+
+  // If not loud enough, return false
+  if (maximum < LOUDNESS_THRESHOLD) {
+    return false;
+  }
+
+  // Determine threshold
+  float thres = THRESHOLD * maximum;
+
+  // Figure out when interesting snippet starts and write to data_out
+  block = PRELENGTH;
+  while (data[block++] < thres);
+  if (block > SIZE_AFTER_FILTER - SNIPPET_SIZE) {
+    block = SIZE_AFTER_FILTER - SNIPPET_SIZE;
+  }
+  for (int i = 0; i < SNIPPET_SIZE; i++) {
+    data_out[i] = data[block-PRELENGTH+i];
+    total += data_out[i];
+  }
+
+  // Normalize data_out
+  for (int i = 0; i < SNIPPET_SIZE; i++) {
+    data_out[i] = data_out[i] / total;
+  }
+
+  return true;
+}
+
+/*---------------------------*/
+/*     Helper functions      */
+/*---------------------------*/
+
+void reset_blinker(void) {
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  digitalWrite(RED_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(100);
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, LOW);
+  delay(100);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, HIGH);
+  delay(100);
+  digitalWrite(GREEN_LED, LOW);
+}
+
+/*---------------------------*/
+/*    Interrupt functions    */
+/*---------------------------*/
+
+// ISR for timestep
+#pragma vector=TIMER2_A0_VECTOR    // Timer A ISR
+__interrupt void Timer2_A0_ISR(void) {
+  if (re_pointer < SIZE) {
+    digitalWrite(RED_LED, HIGH);
+    re[re_pointer] = (analogRead(MIC_INPUT) >> 4) - 128;
+    re_pointer += 1;
+  }
+}
+
+// Set timer for timestep; use A2 since A0 & A1 are used by PWM
+void setTimer(void) {
+  // Set the timer based on 25MHz clock
+  TA2CCR0 = (unsigned int) (25000*ADC_TIMER_MS);
+  TA2CCTL0 = CCIE;
+  __bis_SR_register(GIE);
+  TA2CTL = TASSEL_2 + MC_1 + TACLR + ID_0;
+}
